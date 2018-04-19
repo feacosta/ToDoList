f485bcfd395c30d19fab9f4119329cb6edd454d3
Fernando Acosta <feacosta@alumnos.uai.cl>
2018-04-19 17:10:17 -0300

    Revert "Ex3"
    This reverts commit 166648c6cfc0b6e07b60e7cce2c3dfa46fee60bf.

--- UAI/IX Semestre/POO/Ex3/src/Exercise3.java
+++ UAI/IX Semestre/POO/Ex3/src/Exercise3.java
@@ -5,23 +5,17 @@ public class Exercise3 {
 		  int date = 16;
 		  String month = "July";
 		  int year = 2015;
-		  
+		  //Step 3
 		  System.out.println("Day of the week:"+ day);
 		  System.out.println("Day:"+ date);
 		  System.out.println("Month:" + month);
 		  System.out.println("Year:" + year);
-		  
+		  //Step 4
 		  System.out.println(day+", "+month+" "+date+", "+year);
-		  
+		  //Step 5
 		  System.out.println("American format:");
-		  printAmerican(day, month, date, year);
-		  System.out.println("European format:");
-		  printEuropean(day, date, month, year);
+		  System.out.println(day+", "+month+" "+date+", "+year);
+		  System.out.println("Eurpoean format:");
+		  System.out.println(day+" "+date+" "+month+", "+year);
 		 }
-	 public static void printAmerican(String day,String month, int date, int year){
-		 System.out.println(day+", "+month+" "+date+", "+year);
-	 }
-	 public static void printEuropean(String day, int date, String month, int year){
-		 System.out.println(day+" "+date+" "+month+", "+year);
-	 }
 		}
